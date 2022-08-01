18985d3023accd4550837122b59abd423179fc88
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-08-01 11:28:59 -0700

    added home and dashboard modules with working routes using flask bluwprint in the routes package

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from app.routes import home, dashboard
 
 def create_app(test_config=None):
   # set up app config
@@ -12,5 +12,9 @@ def create_app(test_config=None):
   @app.route('/hello')
   def hello():
     return 'hello world'
+
+  app.register_blueprint(home)
+  app.register_blueprint(dashboard)
+  
 
   return app
