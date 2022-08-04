a0b015df89941e164788a3adbceae843abc59c02
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-08-04 16:22:10 -0700

    set up filter functions as jinja filters in flask app function. Used for formatting data rendered in post-info.html partial

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -5,15 +5,8 @@ def format_date(date):
     return date.strftime('%m/%d/%y')
 
 
-print(format_date(datetime.now()))
-
-
 def format_url(url):
     return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
-
-
-print(format_url('http://google.com/test/'))
-print(format_url('https://www.google.com?q=test'))
 
 
 def format_plural(amount, word):
@@ -21,7 +14,3 @@ def format_plural(amount, word):
         return word + 's'
 
     return word
-
-
-print(format_plural(2, 'cat'))
-print(format_plural(1, 'dog'))
