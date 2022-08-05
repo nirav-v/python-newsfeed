abeab448ecd908b18db3388abe359cc8d655c579
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-08-05 09:33:10 -0700

    signup route succesfully adds new user data from request into the database

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request
+import json
+from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
 
@@ -8,6 +9,17 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 @bp.route('/users', methods=['POST'])
 def signup():
     data = request.get_json()
-    print(data)
+    db = get_db()
 
-    return ''
+    # create a new user
+    newUser = User(
+        username=data['username'],
+        email=data['email'],
+        password=data['password']
+    )
+
+    # save in database
+    db.add(newUser)
+    db.commit()
+
+    return jsonify(id=newUser.id)
