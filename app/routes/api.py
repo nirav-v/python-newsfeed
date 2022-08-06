4dc11dacaa523ae96b33cd982cf9d78b1054f3d2
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-08-06 12:23:56 -0700

    added login_required decorator to the reminain routes that require user to be logged in to access pages

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ import sys
 from flask import Blueprint, request, jsonify, session
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
+from app.utils.auth import login_required
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -66,6 +67,7 @@ def login():
 
 
 @bp.route('/comments', methods=['POST'])
+@login_required
 def comment():
     data = request.get_json()
     db = get_db()
@@ -91,6 +93,7 @@ def comment():
 
 
 @bp.route('/posts/upvote', methods=['PUT'])
+@login_required
 def upvote():
     data = request.get_json()
     db = get_db()
@@ -114,6 +117,7 @@ def upvote():
 
 
 @bp.route('/posts', methods=['POST'])
+@login_required
 def create():
     data = request.get_json()
     db = get_db()
@@ -138,6 +142,7 @@ def create():
 
 
 @bp.route('/posts/<id>', methods=['PUT'])
+@login_required
 def update(id):
     data = request.get_json()
     db = get_db()
@@ -157,6 +162,7 @@ def update(id):
 
 
 @bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
 def delete(id):
     db = get_db()
 
